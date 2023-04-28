073043de188d0cb3740535e51ed8c2d8eda94e52
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2023-04-28 11:17:50 +1000

    Fixed docs

---
+++ doc/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#JLACovarianceMatrix-Documentation","page":"JLACovarianceMatrix","title":"JLACovarianceMatrix Documentation","text":"","category":"section"},{"location":"","page":"JLACovarianceMatrix","title":"JLACovarianceMatrix","text":"","category":"page"},{"location":"#Install","page":"JLACovarianceMatrix","title":"Install","text":"","category":"section"},{"location":"","page":"JLACovarianceMatrix","title":"JLACovarianceMatrix","text":"``` using Pkg Pkg.add(\"JLACovarianceMatrix\")","category":"page"}]
+}
